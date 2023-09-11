986a09ac4cd8af86944ef2a3b60e5be6dd78a1af
Max Marrone <max@marrone.nyc>
2023-09-10 22:50:35 -0400

    Quick hacks to test rendering without virtualization.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { LogEntry, LogIndex, buildIndex, getEntry } from './logAccess'
 
 import { TableVirtuoso } from "react-virtuoso"
 
+import { renderToStaticMarkup } from "react-dom/server"
+
 
 function Datetime({ date }: { date: Date }): JSX.Element {
   const isoString = date.toISOString()
@@ -12,52 +14,66 @@ function Datetime({ date }: { date: Date }): JSX.Element {
 }
 
 function Row({
-  index, file, logIndex
+  entryNumber,
+  rowData
 }: {
-  index: number,
-  file: File,
-  logIndex: LogIndex
+  entryNumber: number
+  rowData: LogEntry
 }): JSX.Element {
-  const [rowData, setRowData] = React.useState(null as null | LogEntry)
+  const priorityClass = [
+    "log-emerg",
+    "log-alert",
+    "log-crit",
+    "log-err",
+    "log-warning",
+    "log-notice",
+    "log-info",
+    "log-debug",
+  ][rowData.priority]
+  return (<tr>
+    <td className={priorityClass}>{entryNumber}</td>
+    <td className={priorityClass}><Datetime date={rowData.timestamp} /></td>
+    <td className={priorityClass}>{rowData.unit}</td>
+    <td className={priorityClass}>{rowData.syslogIdentifier}</td>
+    <td className={priorityClass}><pre>{rowData.message}</pre></td>
+  </tr>)
+}
 
+async function* entries(file: File, index: LogIndex): AsyncGenerator<[number, LogEntry], void>{
+  for (let entryNumber = 0; entryNumber < index.entryCount; entryNumber++) {
+    const entry = await getEntry(file, index, entryNumber)
+    yield [entryNumber, entry]
+  }
+}
+
+async function generateAllRows(file: File, index: LogIndex, container: HTMLElement): Promise<void> {
+  let s = ""
+  for await (const [entryNumber, entry] of entries(file, index)) {
+    const htmlElement = renderToStaticMarkup(<Row entryNumber={entryNumber} rowData={entry} />)
+    s += htmlElement
+    if (entryNumber % 100 == 0) console.log(entryNumber)
+  }
+  container.innerHTML = s
+}
+
+function AllRows({file, index}: {file: File, index: LogIndex}): JSX.Element {
+  console.log("AllRows")
+  const containerRef = React.useRef(null)
   React.useEffect(() => {
-    let ignore = false
-    getEntry(file, logIndex, index).then((entry: LogEntry) => {
-      if (!ignore) setRowData(entry)
-    })
-    return () => { ignore = true }
-    // TODO: Rate-limit this somehow so we don't consume unbounded memory
-    // if the user scrolls really fast?
-  }, [index, file, logIndex])
+    console.log("useEffect")
+    if (containerRef.current != null) {
+      generateAllRows(file, index, containerRef.current)
+    }
+  }, [containerRef])
 
-  if (rowData == null) {
-    return <td colSpan={5}>Loading...</td>
-  }
-  else {
-    const priorityClass = [
-      "log-emerg",
-      "log-alert",
-      "log-crit",
-      "log-err",
-      "log-warning",
-      "log-notice",
-      "log-info",
-      "log-debug",
-    ][rowData.priority]
-    return (<>
-      <td className={priorityClass}>{index}</td>
-      <td className={priorityClass}><Datetime date={rowData.timestamp} /></td>
-      <td className={priorityClass}>{rowData.unit}</td>
-      <td className={priorityClass}>{rowData.syslogIdentifier}</td>
-      <td className={priorityClass}><pre>{rowData.message}</pre></td>
-    </>)
-  }
+  return <tbody ref={containerRef} />
 }
 
 type LogViewState = { status: "indexed", index: LogIndex } | { status: "indexing", progress: number }
 const initialLogViewState: LogViewState = { status: "indexing", progress: 0 }
 
 function LogView({ file }: { file: File | null }): JSX.Element {
+  console.log("LogView")
   const [state, setState] = React.useState<LogViewState>(initialLogViewState)
 
   React.useEffect(() => {
@@ -85,9 +101,8 @@ function LogView({ file }: { file: File | null }): JSX.Element {
   }
   else {
     return (
-      <TableVirtuoso
-        fixedHeaderContent={() => (
-          /* We need to specify an opaque background so the table data isn't visible underneath. */
+      <table style={{ height: '100%', width: '100%' }}>
+        <thead>
           <tr style={{ backgroundColor: "white" }}>
             <td style={{ width: "8ch" }}>#</td>
             <td style={{ width: "16ch" }}>Timestamp</td>
@@ -95,11 +110,9 @@ function LogView({ file }: { file: File | null }): JSX.Element {
             <td style={{ width: "32ch" }}>Syslog ID</td>
             <td>Message</td>
           </tr>
-        )}
-        style={{ height: '100%', width: "100%" }}
-        totalCount={state.index.entryCount}
-        itemContent={(entryNumber) => <Row index={entryNumber} file={file} logIndex={state.index} />}
-      />
+        </thead>
+        <AllRows file={file} index={state.index}/>
+      </table>
     )
   }
 }
@@ -144,7 +157,7 @@ function App() {
 
   return (
     <>
-      <Stopwatch />
+      {/* <Stopwatch /> */}
       <FilePicker setFile={setFile} />
       <LogView file={file} />
     </>
