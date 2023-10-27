6003dc56b2fe7e8c845e9fc866913b45b9beebf6
Max Marrone <max@marrone.nyc>
2023-10-26 22:26:50 -0400

    Make the search bar pretty.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,10 @@
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  MagnifyingGlassIcon,
+} from "@radix-ui/react-icons";
+import { IconButton, Separator, TextField } from "@radix-ui/themes";
+
 // TODO: These are application-level concerns. This component shouldn't care about them.
 export type SearchBarState =
   | {
@@ -22,28 +29,42 @@ export function SearchBar(props: SearchBarProps): JSX.Element {
   const enableButtons =
     props.status === "searchComplete" && props.matchEntryNumbers.length > 0;
   return (
-    <div>
-      <input
-        type="text"
+    <TextField.Root size="3">
+      <TextField.Slot>
+        <MagnifyingGlassIcon />
+      </TextField.Slot>
+      <TextField.Input
+        placeholder="Search messages..."
         onChange={(event) => props.onChange(event.target.value)}
       />
-      {props.status === "searchComplete" ? (
-        props.matchEntryNumbers.length > 0 ? (
-          <span>
-            {props.currentMatchNumber + 1}/{props.matchEntryNumbers.length}
-          </span>
-        ) : (
-          <span>No matches</span>
-        )
-      ) : props.status === "searching" ? (
-        <span>...</span>
-      ) : null}
-      <button onClick={props.onUp} disabled={!enableButtons}>
-        ⬆️
-      </button>
-      <button onClick={props.onDown} disabled={!enableButtons}>
-        ⬇️
-      </button>
-    </div>
+      <TextField.Slot>
+        {props.status === "searchComplete" ? (
+          props.matchEntryNumbers.length > 0 ? (
+            <span style={{ fontVariantNumeric: "tabular-nums" }}>
+              {props.currentMatchNumber + 1}/{props.matchEntryNumbers.length}
+            </span>
+          ) : (
+            <span>No matches</span>
+          )
+        ) : props.status === "searching" ? (
+          <span>...</span>
+        ) : null}
+        <Separator orientation="vertical" />
+        <IconButton
+          onClick={props.onUp}
+          disabled={!enableButtons}
+          variant="ghost"
+        >
+          <ArrowUpIcon />
+        </IconButton>
+        <IconButton
+          onClick={props.onDown}
+          disabled={!enableButtons}
+          variant="ghost"
+        >
+          <ArrowDownIcon />
+        </IconButton>
+      </TextField.Slot>
+    </TextField.Root>
   );
 }
