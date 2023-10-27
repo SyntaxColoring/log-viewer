192124dcc2a1c5a2c04ccfa57ff9097ebc69b4b9
Max Marrone <max@marrone.nyc>
2023-10-26 20:13:47 -0400

    Add Radix top-level boilerplate.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { Theme } from "@radix-ui/themes";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import "@radix-ui/themes/styles.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>,
 );
 
