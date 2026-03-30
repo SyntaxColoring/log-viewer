79bc47acb8cba966ea7058528652a5eb7346f004
Max Marrone <max@marrone.nyc>
2026-03-29 22:58:11 -0400

    refactor: Delete last remnants of Radix.
    The index.css file can also be deleted here because now we have Tailwind's CSS reset, which is doing the same thing that it was.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
-import { Theme } from "@radix-ui/themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "@radix-ui/themes/styles.css";
 import "./index.css";
-import styles from "./index.module.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Theme className={styles.fullSize}>
-      <App />
-    </Theme>
+    <App />
   </React.StrictMode>,
 );
